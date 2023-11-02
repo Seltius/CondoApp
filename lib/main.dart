84868a78d9e071a386a728e336e0837e53627990
José João Dantas <jose_dantas_80@hotmail.com>
2023-11-02 17:13:01 +0000

    Add session validation when app starts

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:condo_app/utils/auth_token.dart';
 import 'package:condo_app/views/apple_page.dart';
 import 'package:condo_app/views/documents_page.dart';
 import 'package:condo_app/views/google_page.dart';
@@ -8,21 +9,24 @@ import 'package:condo_app/views/register_page.dart';
 import 'package:condo_app/views/settings_page.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  bool isAuthenticated = await AuthTokenUtils.isAuthenticated();
+  runApp(MyApp(isAuthenticated: isAuthenticated));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isAuthenticated;
+  const MyApp({super.key, required this.isAuthenticated});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: isAuthenticated ? const PlaceHolderPage() : LoginPage(),
       routes: {
         '/homepage': (context) => const PlaceHolderPage(),
-        '/register': (context) => const RegisterPage(),
+        '/register': (context) => RegisterPage(),
         '/recover': (context) => const RecoverPage(),
         '/documents': (context) => const DocumentsPage(),
         '/settings': (context) => const SettingsPage(),
@@ -32,4 +36,9 @@ class MyApp extends StatelessWidget {
       },
     );
   }
+
+  //TODO
+  // Validate all input fields
+  // Handle api error's
+
 }
---
+++ lib/utils/auth_token.dart
@@ -0,0 +1,43 @@
+import 'package:condo_app/utils/api_endpoints.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthTokenUtils {
+  static Future<String> getToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+    return token ?? '';
+  }
+
+  static Future<void> setToken(String token) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('token', token);
+  }
+
+  static Future<bool> removeToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.remove('token');
+  }
+
+  static Future<bool> isAuthenticated() async {
+    String token = await getToken();
+
+    if (token.isEmpty) {
+      return false;
+    }
+
+    final response = await http.get(
+      Uri.parse(ApiEndPoints.baseUrl + ApiEndPoints.authEndpoints.validate),
+      headers: { 'Authorization': 'Bearer $token' }
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body)['isValid'];
+    } else {
+      return false;
+    }
+
+  }
+
+}
